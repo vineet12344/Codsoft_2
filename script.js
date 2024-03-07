909a1b33bee6b7615e736a72e280c0328b21b688
Vineet Salve <96240536+vineet12344@users.noreply.github.com>
2024-03-08 00:11:11 +0530

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
 const socket = io('http://localhost:3000');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 const messageContainer = document.getElementById("message-container");
 
-const name = prompt('Kimi no Nawa?');
+const name = prompt('Enter your Name: ');
 appendMesage('You Joined');
+
 
 socket.emit('new-user',name);
 
